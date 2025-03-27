5caf904ed06214251bc24f8115d32de1dbc69946
TylerRTDev <TylerRTDev@outlook.com>
2025-03-27 19:20:00 +0000

    Fix: Flask Server now running

--- backend/config.py
+++ backend/config.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 class Config:
     MONGO_URI = os.getenv("MONGO_URI")
-    SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")  # Optional, for forms/auth
+    SECRET_KEY = os.getenv("SECRET_KEY")  # Optional, for forms/auth
     DEBUG = True
